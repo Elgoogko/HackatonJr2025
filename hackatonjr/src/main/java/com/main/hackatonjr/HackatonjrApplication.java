bbe785b2172353199302b629310808ff974b1e0e
Yotacaz <mmjcrissot@laposte.net>
2025-12-04 12:23:38 +0100

    init

---
+++ hackatonjr/src/main/java/com/main/hackatonjr/HackatonjrApplication.java
@@ -0,0 +1,13 @@
+package com.main.hackatonjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonjrApplication.class, args);
+	}
+
+}
---
+++ hackatonjr/src/test/java/com/main/hackatonjr/HackatonjrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.hackatonjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonjrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
