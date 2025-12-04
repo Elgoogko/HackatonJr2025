6110fd2f781a6a28c9a5bc402cbca422ad6a74e0
AtaOFF <133788764+AtaOFF@users.noreply.github.com>
2025-12-04 12:51:24 +0100

    Create Stockables.java

---
+++ hackatonjr/src/main/java/com/main/Stockables.java
@@ -0,0 +1,27 @@
+public class stockables {
+    private String nom;
+    private float prix;
+    public stockables(String nom, float prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public String Getnom() {
+        return nom;
+    }
+    public float Getprix() {
+        return prix;
+    }
+    public void Setnom(String nom) {
+        this.nom = nom;
+    }
+    public void Setprix(float prix) {
+        this.prix = prix;
+    } 
+
+
+
+
+}
+
+
